c7c687e465e83bcd637e256d5ccd2a368ad0a1e3
rhnchowdhury <rhnchdri@gmail.com>
2024-01-08 12:20:06 +0600

    sql added

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Test backend');
 });
+
+app.post('/user', (req, res) => {
+    console.log('api called')
+    console.log(req.body);
+})
 
 app.listen(port, () => {
     console.log(`Backend running on ${port}`);
